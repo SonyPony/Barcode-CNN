a1fe51ee3993cc6e5e5ec9baec3a4f45b52f94c7
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2019-10-09 10:32:48 +0200

    Fix used class instead of object

--- src/train.py
+++ src/train.py
@@ -111,7 +111,7 @@ train_dataset_len = len(train_dataset)
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
 
-model = models[args.model_type]   # type: nn.Module
+model = models[args.model_type]()   # type: nn.Module
 if LOAD_MODEL_PATH:
     start_epoch, best_acc = model_util.load(model, data=torch.load(LOAD_MODEL_PATH))
 
