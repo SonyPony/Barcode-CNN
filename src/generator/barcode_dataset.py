595ac166aa1a32350a9ed29f7ac2b8922c7bf0b9
Son Hai Nguyen <xnguye16@stud.fit.vutbr.cz>
2019-11-11 23:14:58 +0100

    Add generating part in snythetic

--- src/generator/barcode_dataset.py
+++ src/generator/barcode_dataset.py
@@ -9,6 +9,7 @@ from util.dataset import get_file_index, save_crop_with_gt
 import random
 from util import add_noise, random_bright_color
 import util.image as image
+import scipy.ndimage as ndimage
 
 
 static_barcode_options = {
@@ -19,11 +20,15 @@ static_barcode_options = {
 BG_PATH = "C:/Users/Sony/Downloads/VOCdevkit/VOC2012/JPEGImages"
 BG_FILES = os.listdir(BG_PATH)
 BG_COUNT = len(BG_FILES)
-OUT_PATH="../../dataset/syn_rnet_train_data_col_bg/positive"
-IDX = get_file_index(dir=OUT_PATH)
-COUNT = 17000
+OUT_PATH="../../dataset/test3"
+POSITIVE_PATH = "{}/positive".format(OUT_PATH)
+PART_PATH = "{}/part".format(OUT_PATH)
+POSITIVE_IDX = get_file_index(dir=POSITIVE_PATH)
+PART_IDX = get_file_index(dir=PART_PATH)
+COUNT = 20000
 WIN_SIZE = 24
 ANGLE_RANGE = 180
+TRANSLATE = True
 
 
 for i in range(COUNT):
@@ -42,6 +47,7 @@ for i in range(COUNT):
     )
     barcode = add_noise(img=barcode, noise_count=3)
 
+    # RESIZE to rations
     barcode_size = floor(WIN_SIZE * barcode_size_ratio)
     barcode = image.resize(barcode, (barcode_size, barcode_size))
     barcode_mask = image.resize(barcode_mask, (barcode_size, barcode_size))
@@ -64,39 +70,59 @@ for i in range(COUNT):
     offset = None if not TRANSLATE else \
         (random.randint(0, WIN_SIZE - barcode_size), random.randint(0, WIN_SIZE - barcode_size))
 
+    # CREATING PART
+    if barcode_size_ratio > 0.7:
+        base_iou = image.iou_mask(barcode_mask)
+        current_iou = 0.
+
+        part_result, part_result_mask = None, None
+        dx, dy = 0, 0
+        tries = 0
+        while current_iou > 0.8 * base_iou or current_iou < base_iou * 0.5:
+            dy, dx = random.randint(-WIN_SIZE // 3, WIN_SIZE // 3), random.randint(-WIN_SIZE // 3, WIN_SIZE // 3)
+            part_result, part_result_mask = compose_barcode_with_bg(
+                barcode=barcode,
+                background=background,
+                barcode_mask=barcode_mask,
+                translate_vector=(dy, dx)
+            )
+
+            current_iou = image.iou_mask(part_result_mask)
+            tries += 1
+            if tries > 30:
+                print("     Skip part")
+                break
+
+        # save part barcode
+        if tries <= 30:
+            save_crop_with_gt(
+                crop=part_result,
+                crop_mask=part_result_mask,
+                idx=PART_IDX,
+                path=PART_PATH,
+                win_size=WIN_SIZE
+            )
+
+            PART_IDX += 1
+
     result, result_mask = compose_barcode_with_bg(
         barcode=barcode,
         background=background,
         barcode_mask=barcode_mask,
-        translate_vector=offset
+        translate_vector=offset if TRANSLATE else None
     )
-
-    """barcode, mask = random_barcode_with_bg(
-        size_ratio=0 + random.random() / 2.5,
-        rotation_angle=random.randint(-ANGLE_RANGE, ANGLE_RANGE),
-        color=(255, 255, 255) if white_bg else None,
-        module_height=random.random() * 15 + 7,
-        quiet_zone=random.random() * 5 + 1,
-        text_distance=random.random() * 4 + 1,
-        **static_barcode_options
-    )
-
-    # processing and resizing
-    barcode = add_noise(img=barcode, noise_count=3)
-    barcode = image.resize(barcode, (WIN_SIZE, WIN_SIZE))
-    mask = image.resize(mask, (WIN_SIZE, WIN_SIZE))"""
 
     # save image with gt
     save_crop_with_gt(
         crop=result,
         crop_mask=result_mask,
-        idx=IDX,
-        path=OUT_PATH,
+        idx=POSITIVE_IDX,
+        path=POSITIVE_PATH,
         win_size=WIN_SIZE
     )
 
-    print("Generated {}/{} - {:06}.jpg".format(i + 1, COUNT, IDX))
-    IDX += 1
+    print("Generated {}/{} - {:06}.jpg".format(i + 1, COUNT, POSITIVE_IDX))
+    POSITIVE_IDX += 1
 
 
 
